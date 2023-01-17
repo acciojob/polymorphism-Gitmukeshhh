c1c68cc2ce43420f9712da44fcffc4b3fedf4a49
Dell <mukeshdhale27@gmail.com>
2023-01-16 20:51:21 -0800

    sol.

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,13 +1,23 @@
 package com.driver;
 
 public class Main {
+    public static  class  Product{
+        public int product(int x,int y){
+            return x*y;
+        }
+        public  int product(int x,int y,int z){
+            return x*y*z;
+        }
+        public  double product(double x,double y){
+            return x*y;
+        }
+    }
     public  static void main(String[] args){
+             Product p=new Product();
+            p.product(10,5);
+            p.product(10,20,30);
+            p.product(2.5,3.8);
 
     }
 }
 
- class  Product{
-
-
-
- }
